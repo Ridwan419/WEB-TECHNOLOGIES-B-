1a074adb8c8dd9a9620dfb83db121b5a3cd39776
Md Ridwan Islam <19-39432-1@student.aiub.edu>
2021-04-29 16:37:44 +0600

    Lab Task

---
+++ Lab Task 7/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link rel="stylesheet" type="text/css" href="style4.css">
+</head>
+<body>
+
+	<div class="container">
+		<h2 class="a1">About us</h2>
+        <p class="a2">This website is very useful to all kinds of people who are requiring any business dealership. Here you can find your suitable topic. Contact us for queries. Phone: 01971261612, Email: dealershipsystem@gmail.com</p>
+	</div>
+
+</body>
+</html>
